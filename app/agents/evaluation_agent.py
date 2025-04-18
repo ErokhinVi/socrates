9a5c757b01793cf340a764f2ced4501708c09803
Vitaly Erokhin <vitalyerokhin@MacBook-Pro-Vitaly.local>
2025-04-18 18:46:36 +0300

    add star to chat

--- app/agents/evaluation_agent.py
+++ app/agents/evaluation_agent.py
@@ -3,14 +3,27 @@ from app.agents.tools.extract_situation import extract_situation
 from app.agents.tools.extract_task import extract_task
 from app.agents.tools.extract_action import extract_action
 from app.agents.tools.extract_result import extract_result
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class STAROutput(BaseModel):
-    Situation: str
-    Task: str
-    Action: str
-    Result: str
+class HREvaluation(BaseModel):
+    Situation: str = Field(
+        ...,
+        description="Оценка и комментарий по компоненту Situation: указывает, создаёт ли рекрутер контекст ситуации"
+    )
+    Task: str = Field(
+        ...,
+        description="Оценка и комментарий по компоненту Task: уточняет ли рекрутер конкретную задачу кандидата"
+    )
+    Action: str = Field(
+        ...,
+        description="Оценка и комментарий по компоненту Action: стимулирует ли рекрутер кандидата описать свои действия"
+    )
+    Result: str = Field(
+        ...,
+        description="Оценка и комментарий по компоненту Result: затрагивает ли рекрутер результаты и выводы кандидата"
+    )
+
 
 def create_evaluation_agent(system_prompt: str) -> Agent:
     return Agent(
@@ -18,5 +31,5 @@ def create_evaluation_agent(system_prompt: str) -> Agent:
         handoff_description="Оценка сообщений с использованием метода STAR.",
         instructions=system_prompt,
         tools=[extract_situation, extract_action, extract_task, extract_result],
-        output_type=STAROutput
+        output_type=HREvaluation,
     )
--- app/api/interview.py
+++ app/api/interview.py
@@ -12,6 +12,7 @@ from app.agents.prompts.utils import load_prompts
 from pprint import pp
 from agents import Runner
 from app.agents.interviewee_agent import create_interviewee_agent
+from app.agents.evaluation_agent import create_evaluation_agent
 
 # Используем директорию /tmp для временных файлов (доступна для записи всем пользователям)
 TEMP_DIR = "/tmp/ai-interview-temp"
@@ -23,7 +24,14 @@ ttt = TTT()
 stt = STT()
 tts = TTS()
 
-prompts = load_prompts("persona_system_prompt.yaml")
+interviewee_prompts = load_prompts("persona_system_prompt.yaml")
+evaluation_prompts = load_prompts("evaluation_system_prompt.yaml")
+
+
+async def predict_star(agent, history):
+    response = await Runner.run(agent, history)
+    last_evaluation_result = response.final_output_as(cls=dict)
+    return last_evaluation_result
 
 
 # Вебсокет-эндпоинт для интервью
@@ -35,23 +43,28 @@ async def websocket_interview(
 ):
     await ws.accept()  # Принимаем подключение
     # системный промпт для агента на основе выбранной персоны и навыка
-    system_prompt = prompts["persona_system_prompt"].format(
+    system_prompt = interviewee_prompts["persona_system_prompt"].format(
         persona=persona, skill=skill
     )
-    agent = create_interviewee_agent(system_prompt)  # агент для интервью
+    interviewee_agent = create_interviewee_agent(system_prompt)  # агент для интервью
+    star_agent = create_evaluation_agent(evaluation_prompts["evaluation_system_prompt"])
+
     messages = []
     clarify_info = True
+    star_response = {}
     try:
         if clarify_info:
             response = await Runner.run(
-                agent, "Задай уточняющие вопросы по переданному контексту"
+                interviewee_agent, "Задай уточняющие вопросы по переданному контексту"
             )  # Вариант с контекстом
-            agent_text = "Уточняющие вопросы:\n\n" + response.final_output
-            clarify_info = False
+            interviewee_agent_text = "Уточняющие вопросы:\n\n" + response.final_output
+
             messages.append(
-                ttt.create_chat_message(role="assistant", content=agent_text)
+                ttt.create_chat_message(
+                    role="assistant", content=interviewee_agent_text
+                )
             )
-            await ws.send_json({"type": "text", "content": agent_text})
+            await ws.send_json({"type": "text", "content": interviewee_agent_text})
         while True:
             data = await ws.receive_text()  # сообщение от клиента
             json_data = json.loads(data)
@@ -73,32 +86,53 @@ async def websocket_interview(
                 + f"Текст пользователя: {user_input}"
             )
             response = await Runner.run(
-                agent, user_input_w_history
+                interviewee_agent, user_input_w_history
             )  # Вариант с контекстом
-            agent_text = response.final_output  # Текстовый ответ агента
+            interviewee_agent_text = response.final_output  # Текстовый ответ агента
 
             messages.append(ttt.create_chat_message(role="user", content=user_input))
             messages.append(
-                ttt.create_chat_message(role="assistant", content=agent_text)
+                ttt.create_chat_message(
+                    role="assistant", content=interviewee_agent_text
+                )
             )
-
+            if not clarify_info:
+                star_response = await predict_star(
+                    star_agent, ttt.create_history_template(messages)
+                )
+                star_response = star_response.model_dump()
+                print(star_response)
+                # await ws.send_json(
+                #     {"type": "text", "content": str(star_response.model_dump())}
+                # )
+            clarify_info = False
             if is_audio:
                 # Генерируем аудиофайл с ответом агента
                 tts_response = tts.generate_speech(
-                    agent_text, tone=prompts["persona_voice_tone_prompt"]
+                    interviewee_agent_text,
+                    tone=interviewee_prompts["persona_voice_tone_prompt"],
                 )
-                agent_audio = base64.b64encode(tts_response.content).decode("utf-8")
+                interviewee_agent_audio = base64.b64encode(tts_response.content).decode(
+                    "utf-8"
+                )
                 # Отправляем клиенту текст и аудио
                 await ws.send_json(
                     {
                         "type": "voice",
-                        "content": agent_text,
+                        "content": interviewee_agent_text,
                         "user_text": user_input,
-                        "audio": agent_audio,
+                        "audio": interviewee_agent_audio,
+                        "star": star_response,
                     }
                 )
             elif not is_audio:
                 # Отправляем клиенту только текст
-                await ws.send_json({"type": "text", "content": agent_text})
+                await ws.send_json(
+                    {
+                        "type": "text",
+                        "content": interviewee_agent_text,
+                        "star": star_response,
+                    }
+                )
     except WebSocketDisconnect:
         pass
