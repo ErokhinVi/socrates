2acefb6eae0fba0f3307952b780f8a39ce169579
eltarotassadar <bv2000rus42@mail.ru>
2025-04-18 17:02:24 +0300

    first tries and style added

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from app.api.evaluation import router as evaluation_router
 from app.api.interview import router as interview_router
 
 app = FastAPI()
 
+#подключаем стиль
+
+app.mount("/static", StaticFiles(directory="app/frontend"), name="static")
+
 # Инициализируем роутеры для API
 app.include_router(evaluation_router)
 app.include_router(interview_router)
+
 
 # Templates for frontend
 templates = Jinja2Templates(directory="app/frontend")
